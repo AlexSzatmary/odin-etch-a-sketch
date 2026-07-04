e5e11608c68270dd8839b780ed3aa0f727906806
Alex C. Szatmary <alex.szatmary@gmail.com>
2026-07-04 15:02:52 -0400

    Make resize work; random colors

--- etchASketch.js
+++ etchASketch.js
@@ -1,21 +1,179 @@
+COLORS = [
+  "aliceblue",
+  "antiquewhite",
+  "aqua",
+  "aquamarine",
+  "azure",
+  "beige",
+  "bisque",
+  "black",
+  "blanchedalmond",
+  "blue",
+  "blueviolet",
+  "brown",
+  "burlywood",
+  "cadetblue",
+  "chartreuse",
+  "chocolate",
+  "coral",
+  "cornflowerblue",
+  "cornsilk",
+  "crimson",
+  "cyan",
+  "darkblue",
+  "darkcyan",
+  "darkgoldenrod",
+  "darkgray",
+  "darkgreen",
+  "darkkhaki",
+  "darkmagenta",
+  "darkolivegreen",
+  "darkorange",
+  "darkorchid",
+  "darkred",
+  "darksalmon",
+  "darkseagreen",
+  "darkslateblue",
+  "darkslategray",
+  "darkturquoise",
+  "darkviolet",
+  "deeppink",
+  "deepskyblue",
+  "dimgray",
+  "dodgerblue",
+  "firebrick",
+  "floralwhite",
+  "forestgreen",
+  "fuchsia",
+  "gainsboro",
+  "ghostwhite",
+  "gold",
+  "goldenrod",
+  "gray",
+  "green",
+  "greenyellow",
+  "honeydew",
+  "hotpink",
+  "indianred",
+  "indigo",
+  "ivory",
+  "khaki",
+  "lavender",
+  "lavenderblush",
+  "lawngreen",
+  "lemonchiffon",
+  "lightblue",
+  "lightcoral",
+  "lightcyan",
+  "lightgoldenrodyellow",
+  "lightgray",
+  "lightgreen",
+  "lightpink",
+  "lightsalmon",
+  "lightseagreen",
+  "lightskyblue",
+  "lightslategray",
+  "lightsteelblue",
+  "lightyellow",
+  "lime",
+  "limegreen",
+  "linen",
+  "magenta",
+  "maroon",
+  "mediumaquamarine",
+  "mediumblue",
+  "mediumorchid",
+  "mediumpurple",
+  "mediumseagreen",
+  "mediumslateblue",
+  "mediumspringgreen",
+  "mediumturquoise",
+  "mediumvioletred",
+  "midnightblue",
+  "mintcream",
+  "mistyrose",
+  "moccasin",
+  "navajowhite",
+  "navy",
+  "oldlace",
+  "olive",
+  "olivedrab",
+  "orange",
+  "orangered",
+  "orchid",
+  "palegoldenrod",
+  "palegreen",
+  "paleturquoise",
+  "palevioletred",
+  "papayawhip",
+  "peachpuff",
+  "peru",
+  "pink",
+  "plum",
+  "powderblue",
+  "purple",
+  "rebeccapurple",
+  "red",
+  "rosybrown",
+  "royalblue",
+  "saddlebrown",
+  "salmon",
+  "sandybrown",
+  "seagreen",
+  "seashell",
+  "sienna",
+  "silver",
+  "skyblue",
+  "slateblue",
+  "slategray",
+  "snow",
+  "springgreen",
+  "steelblue",
+  "tan",
+  "teal",
+  "thistle",
+  "tomato",
+  "turquoise",
+  "violet",
+  "wheat",
+  "white",
+  "whitesmoke",
+  "yellow",
+  "yellowgreen",
+];
+
+function random_color() {
+  return COLORS[Math.floor(COLORS.length * Math.random())];
+}
+
 const CONTAINER_SIZE = 500;
 container = document.querySelector("#container");
 container.style.width = CONTAINER_SIZE + "px";
 container.style.height = CONTAINER_SIZE + "px";
-// const container_width = parseInt(container.style.width);
 
-let nx = 16;
-const square_size = CONTAINER_SIZE / nx + "px";
-for (let i = 0; i < nx * nx; i++) {
-  const div = document.createElement("div");
-  div.style.width = square_size;
-  div.style.height = square_size;
-  div.style.backgroundColor = "blue";
-  div.style.opacity = "10%";
-  // div.textContent = "asdf";
-  div.addEventListener(
-    "mouseover",
-    () => (div.style.opacity = +div.style.opacity + 0.1),
-  );
-  container.appendChild(div);
+function resize_grid(nx) {
+  container.replaceChildren();
+  const square_size = CONTAINER_SIZE / nx + "px";
+  for (let i = 0; i < nx * nx; i++) {
+    const div = document.createElement("div");
+    div.style.width = square_size;
+    div.style.height = square_size;
+    div.style.backgroundColor = random_color();
+    div.style.opacity = "0%";
+    for (const e of ["mouseover", "touch", "click"]) {
+      div.addEventListener(
+        e,
+        () => (div.style.opacity = +div.style.opacity + 0.1),
+      );
+    }
+    container.appendChild(div);
+  }
 }
+
+btn = document.querySelector("#resizeButton");
+btn.addEventListener("click", () => {
+  nx = prompt("Enter number of cells wide", "16");
+  resize_grid(+nx);
+});
+
+resize_grid(16);
